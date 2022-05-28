b3b018a45794f404b6cf00c553e945c12859f09f
Zachary <75510412+Zachary1575@users.noreply.github.com>
2022-05-28 14:36:13 -0400

    Data base in application.properties, changed unversion files as well

---
+++ services/Complaints/src/main/java/com/example/Complaints/ComplaintsApplication.java
@@ -0,0 +1,14 @@
+package com.example.Complaints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintsApplication {
+
+	// Boot Strapper
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintsApplication.class, args);
+	}
+
+}
